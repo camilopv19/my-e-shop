266655699f0f1050f96f85af13b9257e0a934f7d
Camilo Patiño Velez <cpatino@pactia.com>
2020-04-25 00:36:44 -0500

    First commit, pending to fix the ng-bootstrap module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCqmV-A83E_JsMVrRRZ-KrdRC_nzUcZPbg",
+    authDomain: "melloapp-d2b7a.firebaseapp.com",
+    databaseURL: "https://melloapp-d2b7a.firebaseio.com",
+    projectId: "melloapp-d2b7a",
+    storageBucket: "melloapp-d2b7a.appspot.com",
+    messagingSenderId: "560275448474",
+    appId: "1:560275448474:web:1bed54ebbd591ae451b523",
+    measurementId: "G-N7X8S4LVX1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCqmV-A83E_JsMVrRRZ-KrdRC_nzUcZPbg",
+    authDomain: "melloapp-d2b7a.firebaseapp.com",
+    databaseURL: "https://melloapp-d2b7a.firebaseio.com",
+    projectId: "melloapp-d2b7a",
+    storageBucket: "melloapp-d2b7a.appspot.com",
+    messagingSenderId: "560275448474",
+    appId: "1:560275448474:web:1bed54ebbd591ae451b523",
+    measurementId: "G-N7X8S4LVX1"
+  }
 };
 
 /*
