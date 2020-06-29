b8e7ff27dfccb46addc71d0ecc5d5d55dfa4ae86
Camilo Patiño Velez <cpatino@pactia.com>
2020-06-29 15:58:22 -0500

    Shopping cart icon and service modified. Refactored code, class and interface for the shopping cart are created

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -2,25 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
 import { Router } from '@angular/router';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Observable } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent  {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
   isCollapsed = true;
+  cart$: Observable<ShoppingCart>;
 
-  constructor(private auth: AuthService, private router: Router) {
-    auth.appUser$.subscribe( user => this.appUser = user);
-   }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private cartService: ShoppingCartService) {
+  }
 
-   toggle(){
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(user => this.appUser = user);
+    this.cart$ = await this.cartService.getCart();
+
+  }
+
+  toggle() {
     this.isCollapsed = !this.isCollapsed;
-   }
+  }
 
-  logout(){
+  logout() {
     this.auth.logout();
     this.toggle();
     this.router.navigate(['/login']);
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import 'rxjs/add/operator/switchMap';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products$;
   category: string;
   filteredProducts: any[] = [];
   products: Product[] = [];
+  cart: any;
+  suscription: Subscription;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService
-    ) {
-
+    productService: ProductService,
+    private cartService: ShoppingCartService
+  ) {
     this.products$ = productService.getAll();
 
     this.products$.switchMap(
@@ -36,22 +40,14 @@ export class ProductsComponent {
           this.products;
       });
 
-    // this.products$.subscribe(
-    //   products => {
-    //     this.products = products;
+  }
+  async ngOnInit() {
 
-    //     //route.snapshot.queryParamMap... would refresh the page if the URL changes. It only needs to refresh the DOM current elements
-    //     route.queryParamMap.subscribe(params => {
-    //       this.category = params.get('category');
-
-    //       this.filteredProducts = (this.category) ?
-    //         this.products.filter(p => p.category == this.category) :
-    //         this.products;
-    //     });
-    //   }
-    // );
-
-    
+    this.suscription = (await this.cartService.getCart())
+    .subscribe( cart => this.cart = cart);
   }
 
+  ngOnDestroy(){
+    this.suscription.unsubscribe();
+  }
 }
