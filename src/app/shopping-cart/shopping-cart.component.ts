70fbdf171c3348e8d62720d87d0aa310ad10b44a
camilopv19 <kmilopv@gmail.com>
2020-12-05 16:51:46 -0500

    Products view enhanced: Styles and added a new div for odd size of list. Main css file updated for the card-img-top class which wasn't limiting height

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent {
+export class ShoppingCartComponent implements OnInit{
+  cart$;
 
-  constructor() { }
+  constructor(
+    private cartService: ShoppingCartService) { }
 
+  async ngOnInit() {
+    this.cart$ = await this.cartService.getCart(); //No need to unsuscribe 'cause this is a single-time use item thru the entire app
+    // this.cart$.subscribe( cart => console.log(cart));
+     
+    
+  }
 }
